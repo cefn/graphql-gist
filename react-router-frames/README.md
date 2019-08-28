fde58e9cf5d321d1edf3b916da4bdd95b79751a1
Cefn Hoile <github.com@cefn.com>
2019-08-28 12:52:13 +0100

    Clarity

--- react-router-frames/README.md
+++ react-router-frames/README.md
@@ -1,5 +1,13 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Purpose
+
+It shows an example 'tree' in which there are two 'frames'.
+
+The question is how to add Client-side React routing logic which allows nav links to trigger render changes in different parts of the tree. Currently the react-router implementation is such that navigation affects all subtrees the same.
+
+The idea is to more-or-less duplicate the behaviour of target="_self" if there were a _real_ frameset. In other words, instead of a link clicked in the left frame affecting both the left AND right frames, it would only cause the content in the left frame to navigate.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- react-router-frames/src/FrameSet.js
+++ react-router-frames/src/FrameSet.js
@@ -7,11 +7,11 @@ function Frame(props) {
   </div>
 }
 
-function Nav(props) {
+function View(props) {
   return <div class="pane" style={{ backgroundColor: props.color || "yellow" }} >
-    <p><Link target="_top" to="/red">Choose Red</Link></p>
-    <p><Link target="_top" to="/green">Choose Green</Link></p>
-    <p><Link target="_top" to="/">Reset</Link></p>
+    <p><Link target="_self" to="/red">Choose Red</Link></p>
+    <p><Link target="_self" to="/green">Choose Green</Link></p>
+    <p><Link target="_self" to="/">Reset</Link></p>
   </div>
 }
 
@@ -20,26 +20,26 @@ function FrameSet(props) {
     <Frame name="left">
       <Switch>
         <Route exact path="/" >
-          <Nav />
+          <View />
         </Route>
         <Route path="/green" >
-          <Nav color="green" />
+          <View color="green" />
         </Route>
         <Route path="/red" >
-          <Nav color="red" />
+          <View color="red" />
         </Route>
       </Switch>
     </Frame>
     <Frame name="right">
       <Switch>
         <Route exact path="/" >
-          <Nav />
+          <View />
         </Route>
         <Route path="/green" >
-          <Nav color="green" />
+          <View color="green" />
         </Route>
         <Route path="/red" >
-          <Nav color="red" />
+          <View color="red" />
         </Route>
       </Switch>
     </Frame>
